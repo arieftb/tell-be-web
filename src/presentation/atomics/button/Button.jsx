f0a269a7e9c3c505b035c03f60d17711438d1fa7
Arief T Bagus (TB) <arieftb22@gmail.com>
2025-08-02 16:06:57 +0700

    style: [arieftb] add button atomic

--- src/presentation/atomics/button/Button.jsx
+++ src/presentation/atomics/button/Button.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+import { ParagraphText, SmallText } from '../text/Text'; // Import both text components
 
-function Button (
-  {
-    type = 'button',
-    onClick,
-    disabled = false,
-    children,
-    variant = 'primary'
-  }
-) {
+const Button = ({
+  children,
+  variant = 'primary',
+  size = 'medium',
+  disabled = false,
+  onClick,
+  type = 'button',
+  ...props
+}) => {
+  const TextComponent = size === 'small' ? SmallText : ParagraphText;
+
   return (
     <button
       type={type}
-      onClick={onClick}
+      className={`${styles.button} ${styles[variant]} ${styles[size]}`}
       disabled={disabled}
-      className={`button button-${variant}`}
+      onClick={onClick}
+      {...props}
     >
-      {children}
+      <TextComponent>
+        {children}
+      </TextComponent>
     </button>
   );
-}
+};
 
-Button.defaultProps = {
-  type: PropTypes.oneOf(['button', 'submit', 'reset']),
-  onClick: PropTypes.func,
-  disabled: PropTypes.bool,
+Button.propTypes = {
   children: PropTypes.node.isRequired,
-  variant: PropTypes.oneOf(['primary', 'secondary', 'danger'])
+  variant: PropTypes.oneOf(['primary', 'secondary']),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
 };
 
 export default Button;
