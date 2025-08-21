6b44865379aa3f0c3a5411f29a157cfff79e6130
Arief T Bagus (TB) <arieftb22@gmail.com>
2025-08-21 17:41:17 +0700

    feat: [arieftb] Categories in Home

--- src/presentation/templates/HomeTemplate.jsx
+++ src/presentation/templates/HomeTemplate.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../organisms/header/Header.jsx';
 import ThreadList from '../organisms/thread/ThreadList.jsx';
+import CategorySection from '../organisms/category/CategorySection.jsx';
 import styles from './HomeTemplate.module.css';
 import {Link} from 'react-router-dom';
 
 function HomeTemplate(
-    {threads, status, error, isLoggedIn},
+    {
+      threads, status, error, isLoggedIn, categories,
+      selectedCategory, onSelectCategory,
+    },
 ) {
   return (
     <div className={styles.homeTemplate}>
       <Header/>
       <main className={styles.mainContent}>
+        <CategorySection
+          categories={categories}
+          selectedCategory={selectedCategory}
+          onSelectCategory={onSelectCategory}
+        />
         <ThreadList threads={threads} status={status} error={error}/>
       </main>
       {isLoggedIn && (
@@ -28,6 +37,13 @@ HomeTemplate.propTypes = {
   status: PropTypes.string.isRequired,
   error: PropTypes.string,
   isLoggedIn: PropTypes.bool.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedCategory: PropTypes.string,
+  onSelectCategory: PropTypes.func.isRequired,
+};
+
+HomeTemplate.defaultProps = {
+  selectedCategory: null,
 };
 
 export default HomeTemplate;
